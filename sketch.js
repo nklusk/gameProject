a0198d5c64e7616d19bb379bdcd6a057cd68a46b
NATALIE LUSK <NKL22@fsu.edu>
2023-10-25 14:47:23 -0400

    colorful coding tutorial

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES)
+
+  noLoop()
 }
 
 function draw() {
-  background(125);
+  background(100);
+
+  translate(width / 2, height / 2)
+
+  branch(100)
+}
+
+
+function branch(len) {
+
+  if (len > 10) {
+  line(0, 0, 0, -len)
+  translate(0, -len)
+  rotate(30)
+  branch(len * .7)
+  }
 }
